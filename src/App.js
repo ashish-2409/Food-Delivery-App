01a38068426392278ff8b3fa65ff086ca596df91
Ashish Trehan <ashishtrehan002@gmail.com>
2023-01-12 17:53:10 +0530

    Added components and screens

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Home from './screens/Home';
 function App() {
   return (
-    <div className='fs-1'>
-      Hello World
-    </div>
+    <>
+    <Home/>
+    </>
   );
 }
 
---
+++ src/screens/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from '../components/Header'
+import Footer from '../components/Footer'
+export default function Home() {
+  return (
+    <>
+    <div>
+        <Header/>
+    </div>
+    <div>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Blanditiis omnis excepturi eligendi eum architecto voluptatem, provident praesentium delectus ab magnam ad adipisci sint. Dicta ad temporibus quasi saepe, quas aliquid?</div>
+    <div><Footer/></div>
+    </>
+  )
+}
