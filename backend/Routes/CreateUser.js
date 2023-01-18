1cae55f6ab0c161981d2d70076291e2ab6b9a992
Ashish Trehan <ashishtrehan002@gmail.com>
2023-01-19 00:14:18 +0530

    added create user feature in backend

---
+++ backend/Routes/CreateUser.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const router = express.Router();
+const User = require('../models/User')
+
+router.post('/createuser',async (req,res)=>{
+    try{
+        await User.create({
+            name:req.body.name,
+            password:req.body.password,
+            location:req.body.location,
+            email:req.body.email
+        })
+        res.json({success:true});
+    }
+    catch(error){
+        console.log(error);
+        res.json({success:false});
+    }
+})
+module.exports = router;
--- backend/db.js
+++ backend/db.js
@@ -1,12 +1,21 @@
 const mongoose = require('mongoose');
 const mongouri=require('../src/passwords')// getting password for the mongodb atlas connection
 const mongoDB = async()=>{
-    await mongoose.connect(mongouri,{useNewUrlParser:true},(err,result)=>{
+    await mongoose.connect(mongouri,{useNewUrlParser:true},async(err,result)=>{
         if(err){
             console.log("----",err);
         }
         else{
         console.log("Connected successfully");
+        const fetched_date = await mongoose.connection.db.collection("food_items");
+        fetched_date.find({}).toArray(function(err,data){
+            if(err){
+                console.log(err);
+            }
+            else{
+                console.log(data);
+            }
+        })
         }
     });
 }
